dd9f35b3c4ce3003fa4c5065b0ec915e58463f07
Lisa Vo <lisa.vo97@gmail.com>
2021-11-09 17:05:26 -0800

    First commit

---
+++ ReadMe/ReadMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeApp.swift
+//  ReadMe
+//
+//  Created by Lisa Vo on 11/9/21.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
