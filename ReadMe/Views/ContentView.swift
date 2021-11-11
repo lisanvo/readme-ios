1d1bde273babc9e0aacf6fcf4ab5a0c89a3dd4c2
Lisa Vo <lisa.vo97@gmail.com>
2021-11-10 17:31:36 -0800

    Add BookRow and add edge cases to Image extension

--- ReadMe/Views/ContentView.swift
+++ ReadMe/Views/ContentView.swift
@@ -9,22 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-		 HStack {
-			 Book.Image()
-			 Text("Title")
-				 .font(.title2)
-		 }
+		 BookRow(book: .init())
     }
+}
+
+struct BookRow: View {
+	let book: Book
+	
+	var body: some View {
+		HStack {
+			Book.Image(title: book.title)
+			VStack(alignment: .leading) {
+				Text(book.title)
+					.font(.title2)
+				Text(book.author)
+					.font(.title3)
+					.foregroundColor(.secondary)
+			}
+		}
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
-}
-
-struct Book_Previews: PreviewProvider {
-	static var previews: some View {
-		Book.Image()
-	}
 }
